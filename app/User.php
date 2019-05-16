fd0d4cb50a02734fb36ccd1bc58e4038dd69b30a
Aidan McGourty <ajm67@njit.edu>
2019-05-16 01:36:47 -0700

    Added question migrate, model, and made user seeder and added tests for user profile (cleanup), and question test

--- app/User.php
+++ app/User.php
@@ -63,6 +63,10 @@ class User extends Authenticatable
         return $this->hasOne('App\Profile');
 }
 
+    public function questions(){
+        return $this->hasMany('App\Question');
+    }
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
--- tests/Unit/QuestionTest.php
+++ tests/Unit/QuestionTest.php
@@ -8,19 +8,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class QuestionTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    /** this one
-    public function testSave()
-    {
+    public function testSave(){
         $user = $user = factory(\App\User::class)->make();
         $user->save();
         $question = factory(\App\Question::class)->make();
         $question->user()->associate($user);
         $this->assertTrue($question->save());
+
     }
-     */
 }
